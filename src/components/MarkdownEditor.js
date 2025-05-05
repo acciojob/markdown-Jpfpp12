642e9a11e4088fa6cc559e13f7733f733d89e71b
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-05 12:43:32 +0530

    Update MarkdownEditor.js

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -39,7 +39,7 @@ const MarkdownEditor = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="app flex flex-col h-screen">
       <header className="bg-gray-800 text-white p-4">
         <h1 className="text-xl font-bold">Markdown Editor</h1>
       </header>
